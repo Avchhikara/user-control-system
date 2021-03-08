29fe98d12d3f2b07cffd9d00f90843f6c99412ef
Avnish <Avnishway@gmail.com>
2021-03-08 17:38:45 +0530

    Added duplicate registration prevention logic

--- pages/api/addVehicle.js
+++ pages/api/addVehicle.js
@@ -8,6 +8,15 @@ export default async function login(req, res) {
       userId,
       registrationNumber,
     });
+    // Find if it already exist
+    const v = await Vehicle.find({
+      registrationNumber,
+    });
+    if (v.length > 0) {
+      throw new Error(
+        "Registration number is already registered by someone else!"
+      );
+    }
     await vehicle.save();
     res.status(200).json({ message: "added sucessfully" });
   } catch (err) {
